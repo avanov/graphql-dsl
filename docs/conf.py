7863fc5f6b2b17d7bb7e294b062f41858b628d3a
Maxim Avanov <601955+avanov@users.noreply.github.com>
2020-05-25 19:38:50 +0100

    Release 0.1.2 (#3)
    * Release 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Maxim Avanov'
 author = 'Maxim Avanov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
